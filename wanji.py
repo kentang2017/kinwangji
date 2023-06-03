293602f54a605a8d6b49e090e263b6b46b096f4b
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-06-03 11:52:35 +0800

    Update wanji.py

--- wanji.py
+++ wanji.py
@@ -189,7 +189,7 @@ def display_pan(year, month, day, hour, minute):
 
     yrg_code = [guayaodict.get(i) for i in sixtyfourgua.inverse[yrg][0].replace("6","8").replace("9","7")]
     g1 = "   正卦            運卦            世卦             旬卦            年卦\n"
-    gg = " 【{}】         【{}】        【{}】         【{}】        【{}】\n".format(mg1, yg1, sg1, shg1, yrg1)
+    gg = " 【{}】         【{}】         【{}】         【{}】        【{}】\n".format(mg1, yg1, sg1, shg1, yrg1)
     g2 = " {}         {}         {}         {}         {}\n".format(mg_code[5], yg_code[5], sg_code[5], shg_code[5], yrg_code[5])
     g3 = " {}         {}         {}         {}         {}\n".format(mg_code[4], yg_code[4], sg_code[4], shg_code[4], yrg_code[4])
     g4 = " {}         {}         {}         {}         {}\n".format(mg_code[3], yg_code[3], sg_code[3], shg_code[3], yrg_code[3])
