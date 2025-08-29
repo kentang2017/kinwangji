1e766a3f9c604b7e58882c2a2dd58a9964462fbe
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2025-08-29 16:13:43 +0800

    Add files via upload

--- jieqi.py
+++ jieqi.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May  9 20:32:01 2023
+Created on Wed Aug 27 08:25:17 2025
 
-@author: kentang
+@author: hooki
 """
 
-import re
-from math import pi
-from ephem import Sun, Date, Ecliptic, Equatorial
+import datetime
+from itertools import cycle, repeat
+import  sxtwl
+from sxtwl import fromSolar
 import ephem
 
-jieqi_name = re.findall('..', '春分清明穀雨立夏小滿芒種夏至小暑大暑立秋處暑白露秋分寒露霜降立冬小雪大雪冬至小寒大寒立春雨水驚蟄')
+
+jqmc = ['小寒', '大寒', '立春', '雨水', '驚蟄', '春分', '清明', '穀雨', '立夏', '小滿', '芒種', '夏至', '小暑', '大暑', '立秋', '處暑', '白露', '秋分', '寒露', '霜降', '立冬', '小雪', '大雪', '冬至']
+tian_gan = '甲乙丙丁戊己庚辛壬癸'
+di_zhi = '子丑寅卯辰巳午未申酉戌亥'
+
+#%% 甲子平支
+def jiazi():
+    return list(map(lambda x: "{}{}".format(tian_gan[x % len(tian_gan)],di_zhi[x % len(di_zhi)]),list(range(60))))
+
 
 def multi_key_dict_get(d, k):
     for keys, v in d.items():
@@ -22,124 +31,288 @@ def new_list(olist, o):
     a = olist.index(o)
     res1 = olist[a:] + olist[:a]
     return res1
-
-def ecliptic_lon(jd_utc):
-    return Ecliptic(Equatorial(Sun(jd_utc).ra,Sun(jd_utc).dec,epoch=jd_utc)).lon
-
-def sta(jd_num):
-    return int(ecliptic_lon(jd_num)*180.0/pi/15)
-
-def iteration(jd,sta):
-    s1=sta(jd)
-    s0=s1
-    dt=1.0
+#%% 節氣計算
+def get_jieqi_start_date(year, month, day, hour, minute):
+    """
+    Get the start date and time of the current solar term (jieqi) for the given date and time.
+    Returns a dictionary with year, month, day, hour, minute, and the name of the solar term.
+    """
+    # Initialize the day object with the given date
+    day = sxtwl.fromSolar(year, month, day)
+    
+    # Check if the given date has a solar term
+    if day.hasJieQi():
+        jq_index = day.getJieQi()
+        jd = day.getJieQiJD()
+        t = sxtwl.JD2DD(jd)
+        return {
+            "年": t.Y,
+            "月": t.M,
+            "日": t.D,
+            "時": int(t.h),
+            "分": round(t.m),
+            "節氣": jqmc[jq_index-1],
+            "時間":datetime.datetime(t.Y, t.M, t.D, int(t.h), round(t.m))
+        }
+    else:
+        # If no solar term on this day, find the previous solar term
+        current_day = day
+        while True:
+            current_day = current_day.before(1)
+            if current_day.hasJieQi():
+                jq_index = current_day.getJieQi()
+                jd = current_day.getJieQiJD()
+                t = sxtwl.JD2DD(jd)
+                return {
+                    "年": t.Y,
+                    "月": t.M,
+                    "日": t.D,
+                    "時": int(t.h),
+                    "分": round(t.m),
+                    "節氣": jqmc[jq_index-1],
+                    "時間":datetime.datetime(t.Y, t.M, t.D, int(t.h), round(t.m))
+                }
+            
+def get_before_jieqi_start_date(year, month, day, hour, minute):
+    """
+    Get the start date and time of the next solar term (jieqi) after the given date and time.
+    Returns a dictionary with year, month, day, hour, minute, and the name of the solar term.
+    """
+    # Initialize the day object with the given date
+    day = sxtwl.fromSolar(year, month, day)
+    
+    # Start searching from the next day
+    current_day = day.before(15)
     while True:
-        jd+=dt
-        s=sta(jd)
-        if s0!=s:
-            s0=s
-            dt=-dt/2
-        if abs(dt)<0.0000001 and s!=s1:
-            break
-    return jd
+        if current_day.hasJieQi():
+            jq_index = current_day.getJieQi()
+            jd = current_day.getJieQiJD()
+            t = sxtwl.JD2DD(jd)
+            return {
+                "年": t.Y,
+                "月": t.M,
+                "日": t.D,
+                "時": int(t.h),
+                "分": round(t.m),
+                "節氣": jqmc[jq_index-1],
+                "時間":datetime.datetime(t.Y, t.M, t.D, int(t.h), round(t.m))
+            }
+        current_day = current_day.before(1)
 
-def find_jq_date(year, month, day, hour, jie_qi):
-    jd_format=Date("{}/{}/{} {}:00:00.00".format(str(year).zfill(4), str(month).zfill(2), str(day).zfill(2), str(hour).zfill(2) ))
-    e_1=ecliptic_lon(jd_format)
-    n_1=int(e_1*180.0/pi/15)+1
-    dzlist = []
-    for i in range(24):
-        if n_1>=24:
-            n_1-=24
-        jd_d=iteration(jd_format)
-        d=Date(jd_d+1/3).tuple()
-        bb_1 = {jieqi_name[n_1]: Date("{}/{}/{} {}:{}:00.00".format(str(d[0]).zfill(4), str(d[1]).zfill(2), str(d[2]).zfill(2), str(d[3]).zfill(2) , str(d[4]).zfill(2)))}
-        n_1+=1
-        dzlist.append(bb_1)
-    return list(dzlist[list(map(lambda i:list(i.keys())[0], dzlist)).index(jie_qi)].values())[0]
-
-def find_jq_date1(year, month, day, hour, minute):
-    current = "{}/{}/{} {}:{}:00".format(str(year).zfill(4), str(month).zfill(2), str(day).zfill(2),str(hour).zfill(2), str(minute).zfill(2))
-    changets = Date("{}/{}/{} {}:{}:00".format(str(year).zfill(4), str(month).zfill(2), str(day).zfill(2),str(hour).zfill(2), str(minute).zfill(2)))
-    jd=Date(changets - 24 * ephem.hour *30)
-    e_1=ecliptic_lon(jd)
-    result1 = {}
-    result = []
-    e=ecliptic_lon(jd)
-    n=int(e*180.0/pi/15)+1
-    for i in range(24):
-        if n>=24:
-            n-=24
-        jd=iteration(jd,sta)
-        d=Date(jd+1/3).tuple()
-        dt = "{}/{}/{} {}:{}:00.00".format(d[0],d[1],d[2],str(d[3]).zfill(2),str(d[4]).zfill(2)).split(".")[0]
-        time_info = {  jieqi_name[n]:dt}
-        n+=1    
-        result.append(time_info)
-    for i in result[1:]:
-        result1.update(i)
-    #j = [list(i.keys())[0] for i in result]
-    return result1
+def get_next_jieqi_start_date(year, month, day, hour, minute):
+    """
+    Get the start date and time of the next solar term (jieqi) after the given date and time.
+    Returns a dictionary with year, month, day, hour, minute, and the name of the solar term.
+    """
+    # Initialize the day object with the given date
+    day = sxtwl.fromSolar(year, month, day)
+    
+    # Start searching from the next day
+    current_day = day.after(1)
+    while True:
+        if current_day.hasJieQi():
+            jq_index = current_day.getJieQi()
+            jd = current_day.getJieQiJD()
+            t = sxtwl.JD2DD(jd)
+            return {
+                "年": t.Y,
+                "月": t.M,
+                "日": t.D,
+                "時": int(t.h),
+                "分": round(t.m),
+                "節氣": jqmc[jq_index-1],
+                "時間":datetime.datetime(t.Y, t.M, t.D, int(t.h), round(t.m))
+            }
+        current_day = current_day.after(1)
 
 
-def gong_wangzhuai(j_q):
-    wangzhuai = list("旺相胎沒死囚休廢")
-    #wangzhuai_num = [3,4,9,2,7,6,1,8]
-    wangzhuai_num = list("震巽離坤兌乾坎艮")
-    wangzhuai_jieqi = {('春分','清明','穀雨'):'春分',
-                        ('立夏','小滿','芒種'):'立夏',
-                        ('夏至','小暑','大暑'):'夏至',
-                        ('立秋','處暑','白露'):'立秋',
-                        ('秋分','寒露','霜降'):'秋分',
-                        ('立冬','小雪','大雪'):'立冬',
-                        ('冬至','小寒','大寒'):'冬至',
-                        ('立春','雨水','驚蟄'):'立春'}
-    r1 = dict(zip(new_list(wangzhuai_num, dict(zip(jieqi_name[0::3],wangzhuai_num )).get(multi_key_dict_get(wangzhuai_jieqi, j_q))), wangzhuai))
-    r2 = {v: k for k, v in r1.items()}
-    return r1, r2
+def jq(year, month, day, hour, minute):
+    """
+    Get the current solar term (jieqi) for the given date and time.
+    Returns the name of the solar term as a string.
+    """
+    try:
+        current_datetime = datetime.datetime(year, month, day, hour, minute)
+        jq_start_dict = get_jieqi_start_date(year, month, day, hour, minute)
+        next_jq_start_dict = get_next_jieqi_start_date(year, month, day, hour, minute)
+        
+        if not (isinstance(jq_start_dict, dict) and isinstance(next_jq_start_dict, dict) and 
+                "時間" in jq_start_dict and "時間" in next_jq_start_dict and
+                "節氣" in jq_start_dict and "節氣" in next_jq_start_dict):
+            raise ValueError(f"Invalid jieqi dictionary format for {year}-{month}-{day} {hour}:{minute}")
+        
+        jq_start_datetime = jq_start_dict["時間"]
+        next_jq_start_datetime = next_jq_start_dict["時間"]
+        jq_name = jq_start_dict["節氣"]
+        
+        if not (isinstance(jq_start_datetime, datetime.datetime) and isinstance(next_jq_start_datetime, datetime.datetime)):
+            raise ValueError(f"Jieqi times are not datetime objects: {jq_start_datetime}, {next_jq_start_datetime}")
+        
+        # Check if current_datetime is within the current jieqi period
+        if jq_start_datetime <= current_datetime < next_jq_start_datetime:
+            return jq_name
+        # If before the current jieqi start, get the previous jieqi
+        elif current_datetime < jq_start_datetime:
+            prev_jq_start_dict = get_before_jieqi_start_date(year, month, day, hour, minute)
+            if not (isinstance(prev_jq_start_dict, dict) and "節氣" in prev_jq_start_dict):
+                raise ValueError(f"Invalid previous jieqi dictionary format for {year}-{month}-{day}")
+            return prev_jq_start_dict["節氣"]
+        else:
+            raise ValueError(f"Current datetime {current_datetime} not within any valid jieqi period")
+    except Exception as e:
+        raise ValueError(f"Error in jq for {year}-{month}-{day} {hour}:{minute}: {str(e)}")
+
+def ke_jiazi_d(hour):
+    t = [f"{h}:{m}0" for h in range(24) for m in range(6)]
+    minutelist = dict(zip(t, cycle(repeat_list(1, find_lunar_ke(hour)))))
+    return minutelist
+
+def repeat_list(n, thelist):
+    return [repetition for i in thelist for repetition in repeat(i,n)]
 
 
-def xzdistance(year, month, day, hour):
-    return int(find_jq_date(year, month, day, hour, "夏至") -  Date("{}/{}/{} {}:00:00.00".format(str(year).zfill(4), str(month).zfill(2), str(day).zfill(2), str(hour).zfill(2))))
+#五虎遁，起正月
+def find_lunar_month(year):
+    fivetigers = {
+    tuple(list('甲己')):'丙寅',
+    tuple(list('乙庚')):'戊寅',
+    tuple(list('丙辛')):'庚寅',
+    tuple(list('丁壬')):'壬寅',
+    tuple(list('戊癸')):'甲寅'
+    }
+    if multi_key_dict_get(fivetigers, year[0]) == None:
+        result = multi_key_dict_get(fivetigers, year[1])
+    else:
+        result = multi_key_dict_get(fivetigers, year[0])
+    return dict(zip(range(1,13),new_list(jiazi(), result)[:12]))
 
-def distancejq(year, month, day, hour, jq):
-    return int( Date("{}/{}/{} {}:00:00.00".format(str(year).zfill(4), str(month).zfill(2), str(day).zfill(2), str(hour).zfill(2))) - find_jq_date(year-1, month, day, hour, jq) )
+#五鼠遁，起子時
+def find_lunar_hour(day):
+    fiverats = {
+    tuple(list('甲己')):'甲子',
+    tuple(list('乙庚')):'丙子',
+    tuple(list('丙辛')):'戊子',
+    tuple(list('丁壬')):'庚子',
+    tuple(list('戊癸')):'壬子'
+    }
+    if multi_key_dict_get(fiverats, day[0]) == None:
+        result = multi_key_dict_get(fiverats, day[1])
+    else:
+        result = multi_key_dict_get(fiverats, day[0])
+    return dict(zip(list(di_zhi), new_list(jiazi(), result)[:12]))
 
-def fjqs(year, month, day, hour):
-    jd_format = Date("{}/{}/{} {}:00:00.00".format(str(year).zfill(4), str(month).zfill(2), str(day).zfill(2), str(hour).zfill(2) ))
-    n= int(ecliptic_lon(jd_format)*180.0/pi/15)+1
-    c = []
-    for i in range(1):
-        if n>=24:
-            n-=24
-        d = Date(jd_format+1/3).tuple()
-        c.append([jieqi_name[n], Date("{}/{}/{} {}:{}:00.00".format(str(d[0]).zfill(4), str(d[1]).zfill(2), str(d[2]).zfill(2), str(d[3]).zfill(2) , str(d[4]).zfill(2)))])
-    return c[0]
+#五馬遁，起子刻
+def find_lunar_ke(hour):
+    fivehourses = {
+    tuple(list('丙辛')):'甲午',
+    tuple(list('丁壬')):'丙午',
+    tuple(list('戊癸')):'戊午',
+    tuple(list('甲己')):'庚午',
+    tuple(list('乙庚')):'壬午'
+    }
+    if multi_key_dict_get(fivehourses, hour[0]) == None:
+        result = multi_key_dict_get(fivehourses, hour[1])
+    else:
+        result = multi_key_dict_get(fivehourses, hour[0])
+    return new_list(jiazi(), result)
 
-def jq(year, month, day, hour, minute):#从当前时间开始连续输出未来n个节气的时间
-    #current =  datetime.strptime("{}/{}/{} {}:{}:00".format(str(year).zfill(4), str(month).zfill(2), str(day).zfill(2),str(hour).zfill(2), str(minute).zfill(2)), '%Y/%m/%d %H:%M:%S')
-    current = Date("{}/{}/{} {}:{}:00".format(str(year).zfill(4), str(month).zfill(2), str(day).zfill(2),str(hour).zfill(2), str(minute).zfill(2)))
-    changets = Date("{}/{}/{} {}:{}:00".format(str(year).zfill(4), str(month).zfill(2), str(day).zfill(2),str(hour).zfill(2), str(minute).zfill(2)))
-    jd=Date(changets - 24 * ephem.hour *30)
-    #jd = Date("{}/{}/{} {}:{}:00.00".format(str(b.year).zfill(4), str(b.month).zfill(2), str(b.day).zfill(2), str(b.hour).zfill(2), str(b.minute).zfill(2)  ))
-    result = []
-    e=ecliptic_lon(jd)
-    n=int(e*180.0/pi/15)+1
-    for i in range(3):
-        if n>=24:
-            n-=24
-        jd=iteration(jd,sta)
-        d=Date(jd+1/3).tuple()
-        dt = Date("{}/{}/{} {}:{}:00.00".format(d[0],d[1],d[2],d[3],d[4]).split(".")[0])
-        time_info = {  dt:jieqi_name[n]}
-        n+=1    
-        result.append(time_info)
-    j = [list(i.keys())[0] for i in result]
-    if current > j[0] and current > j[1] and current > j[2]:
-        return list(result[2].values())[0]
-    if current > j[0] and current > j[1] and current <= j[2]:
-        return list(result[1].values())[0]
-    if current >= j[1] and current < j[2]:
-        return list(result[1].values())[0]
-    if current < j[1] and current < j[2]:
-        return list(result[0].values())[0]
+#農曆
+def lunar_date_d(year, month, day):
+    lunar_m = ['占位', '正月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '冬月', '腊月']
+    day = fromSolar(year, month, day)
+    return {"年":day.getLunarYear(),
+            "農曆月": lunar_m[int(day.getLunarMonth())],
+            "月":day.getLunarMonth(),
+            "日":day.getLunarDay()}
+
+#換算干支
+def gangzhi1(year, month, day, hour, minute):
+    if hour == 23:
+        d = ephem.Date(round((ephem.Date("{}/{}/{} {}:00:00.00".format(
+            str(year).zfill(4),
+            str(month).zfill(2),
+            str(day+1).zfill(2),
+            str(0).zfill(2)))),3))
+    else:
+        d = ephem.Date("{}/{}/{} {}:00:00.00".format(
+            str(year).zfill(4),
+            str(month).zfill(2),
+            str(day).zfill(2),
+            str(hour).zfill(2)))
+    dd = list(d.tuple())
+    cdate = fromSolar(dd[0], dd[1], dd[2])
+    yTG,mTG,dTG,hTG = "{}{}".format(
+        tian_gan[cdate.getYearGZ().tg],
+        di_zhi[cdate.getYearGZ().dz]), "{}{}".format(
+            tian_gan[cdate.getMonthGZ().tg],
+            di_zhi[cdate.getMonthGZ().dz]), "{}{}".format(
+                tian_gan[cdate.getDayGZ().tg],
+                di_zhi[cdate.getDayGZ().dz]), "{}{}".format(
+                    tian_gan[cdate.getHourGZ(dd[3]).tg],
+                    di_zhi[cdate.getHourGZ(dd[3]).dz])
+    if year < 1900:
+        mTG1 = find_lunar_month(yTG).get(lunar_date_d(year, month, day).get("月"))
+    else:
+        mTG1 = mTG
+    hTG1 = find_lunar_hour(dTG).get(hTG[1])
+    return [yTG, mTG1, dTG, hTG1]
+
+def gangzhi(year, month, day, hour, minute):
+    if hour == 23:
+        d = ephem.Date(round((ephem.Date("{}/{}/{} {}:00:00.00".format(
+            str(year).zfill(4),
+            str(month).zfill(2),
+            str(day+1).zfill(2),
+            str(0).zfill(2)))),3))
+    else:
+        d = ephem.Date("{}/{}/{} {}:00:00.00".format(
+            str(year).zfill(4),
+            str(month).zfill(2),
+            str(day).zfill(2),
+            str(hour).zfill(2)))
+    dd = list(d.tuple())
+    cdate = fromSolar(dd[0], dd[1], dd[2])
+    yTG,mTG,dTG,hTG = "{}{}".format(
+        tian_gan[cdate.getYearGZ().tg],
+        di_zhi[cdate.getYearGZ().dz]), "{}{}".format(
+            tian_gan[cdate.getMonthGZ().tg],
+            di_zhi[cdate.getMonthGZ().dz]), "{}{}".format(
+                tian_gan[cdate.getDayGZ().tg],
+                di_zhi[cdate.getDayGZ().dz]), "{}{}".format(
+                    tian_gan[cdate.getHourGZ(dd[3]).tg],
+                    di_zhi[cdate.getHourGZ(dd[3]).dz])
+    mTG1 = find_lunar_month(yTG).get(lunar_date_d(year, month, day).get("月"))
+    hTG1 = find_lunar_hour(dTG).get(hTG[1])
+    zi = gangzhi1(year, month, day, 0, 0)[3]
+    if minute < 10 and minute >=0:
+        reminute = "00"
+    if minute < 20 and minute >=10:
+        reminute = "10"
+    if minute < 30 and minute >=20:
+        reminute = "20"
+    if minute < 40 and minute >=30:
+        reminute = "30"
+    if minute < 50 and minute >=40:
+        reminute = "40"
+    if minute < 60 and minute >=50:
+        reminute = "50"
+    hourminute = str(hour)+":"+str(reminute)
+    gangzhi_minute = ke_jiazi_d(zi).get(hourminute)
+    return [yTG, mTG1, dTG, hTG1, gangzhi_minute]
+        
+
+if __name__ == '__main__':
+    year = 2005
+    month = 5
+    day = 5
+    hour = 16
+    minute = 30
+    #print(liujiashun_dict())
+    #print(qimen_ju_name_zhirun_raw(year, month, day, hour, minute))
+    print(f"{year}-{month}-{day} {hour}:{minute}")
+    #print( get_jieqi_start_date(year, month, day, hour, minute))
+    #print( get_next_jieqi_start_date(year, month, day, hour, minute))
+    #print( get_before_jieqi_start_date(year, month, day, hour, minute))
+    print(gangzhi(year, month, day, hour, minute))
+    #print(find_lunar_month(gangzhi(year, month, day, hour, minute)[0]))
+        
