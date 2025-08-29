054b1a3a09c223e615a265adb4116c4e45fcfbe7
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2025-08-29 16:25:44 +0800

    Update jieqi.py

--- jieqi.py
+++ jieqi.py
@@ -5,7 +5,7 @@ Created on Wed Aug 27 08:25:17 2025
 @author: hooki
 """
 
-import datetime
+import datetime, re
 from itertools import cycle, repeat
 import  sxtwl
 from sxtwl import fromSolar
@@ -334,4 +334,5 @@ if __name__ == '__main__':
     #print(find_lunar_month(gangzhi(year, month, day, hour, minute)[0]))
 
         
+
 
